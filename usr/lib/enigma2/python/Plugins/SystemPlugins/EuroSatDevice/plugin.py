b75d9d42f5d00fa715c8745cccdbe6e43ed7bb1a
eurosata1 <eurosatA.1@gmail.com>
2015-03-17 22:18:49 +0100

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/SystemPlugins/EuroSatDevice/plugin.py
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/EuroSatDevice/plugin.py
@@ -1,4 +1,18 @@
-# for localized messages
+#
+#  EuroSatDevice Mod. EuroS@t
+#  thx Coded by Dr.Best (c)2010
+#  Support: www.dreambox-tools.info
+#
+#  This plugin is licensed under the Creative Commons 
+#  Attribution-NonCommercial-ShareAlike 3.0 Unported 
+#  License. To view a copy of this license, visit
+#  http://creativecommons.org/licenses/by-nc-sa/3.0/ or send a letter to Creative
+#  Commons, 559 Nathan Abbott Way, Stanford, California 94305, USA.
+#
+#  This plugin is NOT free software. It is open source, you are allowed to
+#  modify it (if you keep the license), but it may not be commercially 
+#  distributed.
+#
 from . import _
 
 from HddSetup import HddSetup
